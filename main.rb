9455b4e1dc400a88090f418d4cca181a6f3106d8
Luke Reynolds <lukereynoldspi@gmail.com>
2021-12-08 11:52:07 -0800

    Resolved issue 4

--- main.rb
+++ main.rb
@@ -2,12 +2,12 @@ require_relative 'game'
 require_relative 'box'
 require_relative 'die'
 
-NUM_T = 9
+N_TILES = 9
 N_DICE = 2
 N_SIDES = 6
 
 dice = (1..N_DICE).map { |i| Die.new(N_SIDES) }
-box = Box.new(NUM_T)
+box = Box.new(NUM_TILES)
 game = Game.new(box, dice)
 
 game.prepare_next_round
