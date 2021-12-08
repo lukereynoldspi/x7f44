93cc19c7cf6ea6beaddcf82f18e677f3c65335f5
Luke Reynolds <lukereynoldspi@gmail.com>
2021-12-08 11:31:19 -0800

    Resolved issue #2, deleted commented code as well

--- die.rb
+++ die.rb
@@ -2,28 +2,16 @@ class Die
 
   attr_reader :number_of_sides
   attr_accessor :value
-  attr_reader :prng
+  attr_reader :random
 
   def initialize(number_of_sides)
     @number_of_sides = number_of_sides
     @value = 1
-    @prng = Random.new
+    @random = Random.new
   end
 
   def roll!
-    self.value = prng.rand(1..number_of_sides)
+    self.value = random.rand(1..number_of_sides)
   end
-
-  def makeRoll
-        roll!
-    end
-
-#
-#
-  # def to_s
-  #   value
-  # end
-#
-# end
 
 end
